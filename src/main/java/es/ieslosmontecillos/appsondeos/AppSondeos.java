a96c86b42548ab7b5e0a810b13eb1aa2bd7f6d47
alefdez99 <alejandrofernandezbarrionuevo@gmail.com>
2023-10-11 13:36:37 +0200

    Version 1.0 Ale Fdez - First Commit. Developing Classes Sports and Journeys.

--- src/main/java/es/ieslosmontecillos/appsondeos/AppSondeos.java
+++ src/main/java/es/ieslosmontecillos/appsondeos/AppSondeos.java
@@ -6,11 +6,9 @@
 package es.ieslosmontecillos.appsondeos;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,25 +16,24 @@ import javafx.stage.Stage;
  * @author Profesora
  */
 public class AppSondeos extends Application {
-    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage)
+    {
+        TabPane mainTab = new TabPane();
+
+        Sports sports = new Sports();
+        mainTab.getTabs().add(sports);
+
+        Journeys journeys = new Journeys();
+        mainTab.getTabs().add(journeys);
+
+        GridPane root = new GridPane();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Scene scene = new Scene(root, 800, 700);
+
+        root.getChildren().add(mainTab);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Encuesta");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
