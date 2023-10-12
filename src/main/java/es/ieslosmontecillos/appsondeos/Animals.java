40e3317a1f23ee16b11a5324c61582eab7020568
Diego <diego02andrade@gmail.com>
2023-10-12 15:09:19 +0200

    Commit of new branch Diego

---
+++ src/main/java/es/ieslosmontecillos/appsondeos/Animals.java
@@ -0,0 +1,14 @@
+package es.ieslosmontecillos.appsondeos;
+
+import javafx.scene.layout.GridPane;
+
+public class Animals {
+    private GridPane grid;
+    public Animals(){
+        makeGUI();
+    }
+
+    public void makeGUI(){
+
+    }
+}
